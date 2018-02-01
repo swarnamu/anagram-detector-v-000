efbd0fc7685c0fd2ca15260b01fce96703dac82d
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-02-01 18:18:03 +0000

    updated anagram class

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,12 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :words
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(some_word)
+    some_word.select { |letters| letters.split("").sort == @word.split("").sort  }
+  end
+end
