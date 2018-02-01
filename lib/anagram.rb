62d743a9caf9515db5846292a1e7e6e3d3e56c19
Swarna Muralidharan <swarna.muralic@gmail.com>
2018-02-01 18:15:27 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,12 +1,12 @@
 # Your code goes here!
 class listen
   attr_accessor :words
-  @@words = []
+
   def initialize(word)
     @word = word
   end
 
-  def self.match(some_word)
+  def match(some_word)
     some_word.select { |letters| letters.split("").sort == @word.split("").sort  }
   end
 end
